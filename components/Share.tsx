b24bdfbf9eb6cc8ca9a790f64b21496d671359d4
na2hiro <a@81.la>
2020-05-31 22:41:30 +0900

    Tap to copy link

--- components/Share.tsx
+++ components/Share.tsx
@@ -1,5 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { Teban } from "shogitter-ts/lib/Teban";
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+const CopyableInput = ({value}) => {
+    const [copied, setCopied] = useState(false);
+    const ref = useRef<HTMLInputElement>();
+    return <>
+        <CopyToClipboard text={value} onCopy={() => {setCopied(true); ref?.current?.select(); }}>
+            <input value={value} size={27} readOnly style={{maxWidth: "80%"}} ref={ref} onBlur={() => setCopied(false)}/>
+        </CopyToClipboard> {copied && "Copied!"}
+    </>
+}
 
 const Share = ({role, tokens}) => {
     const [currentUrl, setCurrentUrl] = useState("");
@@ -9,15 +20,14 @@ const Share = ({role, tokens}) => {
     return <>
         <h2>🔗 Share</h2>
         <ul style={{listStyle: "none", paddingLeft: "10px"}}>
-            <li>👀 watch <input value={`${currentUrl}#`} size={50} readOnly style={{maxWidth: "80%"}}/></li>
+            <li>👀 watch <CopyableInput value={`${currentUrl}`} /></li>
             {role === "owner" && <>
-                <li>✍️️ edit <input value={`${currentUrl}/edit/${tokens.edit}`} size={50} readOnly
-                                   style={{maxWidth: "80%"}}/></li>
+                <li>✍️️ edit <CopyableInput value={`${currentUrl}/edit/${tokens.edit}`} /></li>
                 <li style={{opacity: "0.3"}}>🤝 play
                     <ul>
-                        <li>{Teban.getMark(0)} <input value={`#under-construction`} size={50} readOnly
+                        <li>{Teban.getMark(0)} <input value={`#under-construction`} size={27} readOnly
                                    style={{maxWidth: "80%"}}/></li>
-                        <li>{Teban.getMark(1)} <input value={`#under-construction`} size={50} readOnly
+                        <li>{Teban.getMark(1)} <input value={`#under-construction`} size={27} readOnly
                                    style={{maxWidth: "80%"}}/></li>
                     </ul>
                 </li>
